9cb90bbda2d52b663106001dba67798dbb84e09a
justsaykk <kaikein.woo@gmail.com>
2022-07-01 12:21:53 +0800

    surronded with try-catch

--- task02/src/main/java/assessment/Client.java
+++ task02/src/main/java/assessment/Client.java
@@ -13,47 +13,50 @@ import java.util.List;
 public class Client {
 
     public static void main(String[] args) throws UnknownHostException, IOException {
-        Socket sock = new Socket("task02.chuklee.com", 80);
-        List<Integer> list = new ArrayList<>();
+        try (Socket sock = new Socket("task02.chuklee.com", 80)) {
+            List<Integer> list = new ArrayList<>();
 
-        // Getting Streams:
-        OutputStream out = sock.getOutputStream();
-        InputStream in = sock.getInputStream();
-        ObjectOutputStream oos = new ObjectOutputStream(out);
-        ObjectInputStream ois = new ObjectInputStream(in);
+            // Getting Streams:
+            OutputStream out = sock.getOutputStream();
+            InputStream in = sock.getInputStream();
+            ObjectOutputStream oos = new ObjectOutputStream(out);
+            ObjectInputStream ois = new ObjectInputStream(in);
 
-        // Read and isolate initial Request from server
-        String res = ois.readUTF();
-        String[] split = res.split(" ");
-        String requestID = split[0];
-        String[] numbers = split[1].split(",");
+            // Read and isolate initial Request from server
+            String res = ois.readUTF();
+            String[] split = res.split(" ");
+            String requestID = split[0];
+            String[] numbers = split[1].split(",");
 
-        for (String el : numbers) {
-            list.add(Integer.parseInt(el));
-        }
+            for (String el : numbers) {
+                list.add(Integer.parseInt(el));
+            }
 
-        // Passing stuff to calculator
-        Calculator task = new Calculator();
-        Float average = task.calculate(list);
+            // Passing stuff to calculator
+            Calculator task = new Calculator();
+            Float average = task.calculate(list);
 
-        // Write back to server
-        String name = "Woo Kai Kein";
-        String email = "kaikein.woo@gmail.com";
-        oos.writeUTF(requestID);
-        oos.writeUTF(name);
-        oos.writeUTF(email);
-        oos.writeFloat(average);
-        oos.flush();
+            // Write back to server
+            String name = "Woo Kai Kein";
+            String email = "kaikein.woo@gmail.com";
+            oos.writeUTF(requestID);
+            oos.writeUTF(name);
+            oos.writeUTF(email);
+            oos.writeFloat(average);
+            oos.flush();
 
-        // Read response from server
-        if (ois.readBoolean()) {
-            System.out.println("SUCCESS");
-            out.close();
-            in.close();
-        } else {
-            System.out.println("FAILED: " + ois.readUTF());
-            out.close();
-            in.close();
+            // Read response from server
+            if (ois.readBoolean()) {
+                System.out.println("SUCCESS");
+                out.close();
+                in.close();
+            } else {
+                System.out.println("FAILED: " + ois.readUTF());
+                out.close();
+                in.close();
+            }
+        } catch (NumberFormatException e) {
+            e.printStackTrace();
         }
     }
 }
